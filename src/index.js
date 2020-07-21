67a99d2d4a91daa13f65f91c0f9899f311888dea
jstn027 <chuajustin27@gmail.com>
2020-07-21 15:58:31 +0800

    updated index.js/index.test.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const functions = {
   gradeClassifier: (grade) => {
-    // TODO: Write code here
+    if(grade >= 0 && grade <= 74.99)
+      return 'D'
+    else if(grade >= 75.00 && grade <= 79.99)
+      return 'N'
+    else if(grade >= 80.00 && grade <= 84.99)
+      return 'S'
+    else if(grade >= 85.00 && grade <= 89.99)
+      return 'G'
+    else if(grade >= 90.00 && grade <= 94.99)
+      return 'V'
+    else if(grade >= 95.00 && grade <= 100.00)
+      return 'O'
+    else
+      return "Invalid"
   }
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,57 @@
 const { gradeClassifier } = require('./index');
 
-// TODO: Write unit tests here
 describe('Scenario', () => {
-  it('When ..., then ...', () => {
-    // Arrange
+  it('When Grade is between 0 to 74.99, then the letter grade should be D (Did not Meet Expectation).', () => {
+    var grade = 74.99
+    var result = gradeClassifier(grade)
+    expect(result).toBe('D')
+  });
 
-    // Act
+  it('When Grade is between 75.00 to 79.99, then the letter grade should be N (Needs Improvement).', () => {
+    var grade = 79.99
+    var result = gradeClassifier(grade)
+    expect(result).toBe('N')
+  });
 
-    // Assert
+  it('When Grade is between 80.00 to 84.99, then the letter grade should be S (Satisfactory).', () => {
+    var grade = 84.99
+    var result = gradeClassifier(grade)
+    expect(result).toBe('S')
+  });
+
+  it('When Grade is between 85.00 to 89.99, then the letter grade should be G (Good).', () => {
+    var grade = 89.99
+    var result = gradeClassifier(grade)
+    expect(result).toBe('G')
+  });
+
+  it('When Grade is between 90.00 to 94.99, then the letter grade should be V (Very Good).', () => {
+    var grade = 94.99
+    var result = gradeClassifier(grade)
+    expect(result).toBe('V')
+  });
+
+  it('When Grade is between 95.00 to 100, then the letter grade should be O (Outstanding).', () => {
+    var grade = 100
+    var result = gradeClassifier(grade)
+    expect(result).toBe('O')
+  });
+
+  it('When Grade is Less than 0, then the result should be "Invalid".', () => {
+    var grade = -1
+    var result = gradeClassifier(grade)
+    expect(result).toBe('Invalid')
+  });
+
+  it('When Grade is Greater than 100, then the result should be "Invalid".', () => {
+    var grade = 101
+    var result = gradeClassifier(grade)
+    expect(result).toBe('Invalid')
+  });
+
+  it('When Grade is "NaN" or "Not a Number", then the result should be "Invalid".', () => {
+    var grade = 'Word Not Number'
+    var result = gradeClassifier(grade)
+    expect(result).toBe('Invalid')
   });
 });
